8924a3df8d41061b9fcaa1736b04e8d0855157c4
HoangLong-Lumi <111274514+HoangLong-Lumi@users.noreply.github.com>
2022-10-09 20:19:32 +0700

    a32: Initial device tree Signed-off-by: Nguyễn Long <hoanglong.lumi404@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -9,5 +9,11 @@ include device/samsung/mt6769-common/BoardConfigCommon.mk
 
 DEVICE_PATH := device/samsung/a32
 
+# Kernel
+TARGET_PREBUILT_KERNEL := $(DEVICE_PATH)/prebuilt/Image.gz
+TARGET_PREBUILT_DTB := $(DEVICE_PATH)/prebuilt/dtb
+BOARD_PREBUILT_DTBOIMAGE := $(DEVICE_PATH)/prebuilt/dtbo.img
+BOARD_INCLUDE_RECOVERY_DTBO := true
+
 # Inherit from the proprietary version
 include vendor/samsung/a32/BoardConfigVendor.mk
