efe3a6fc9bec1c402bfec7cebd4633bd7d8e5010
HoangLong-Lumi <111274514+HoangLong-Lumi@users.noreply.github.com>
2022-10-29 21:21:04 +0700

    a32: Import NFC from stock

--- device.mk
+++ device.mk
@@ -7,5 +7,15 @@
 # Inherit from common
 $(call inherit-product, device/samsung/mt6769-common/mt6769.mk)
 
+# NFC
+PRODUCT_PACKAGES += \
+    com.android.nfc_extras \
+    android.hardware.nfc@1.2-service \
+    NfcNci \
+    Tag
+
+PRODUCT_COPY_FILES += \
+    $(DEVICE_PATH)/configs/nfc/libnfc-sec-vendor.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-sec-vendor.conf
+
 # Inherit from vendor if exists
 $(call inherit-product-if-exists, vendor/samsung/a32/a32-vendor.mk)
