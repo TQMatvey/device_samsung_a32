2b2a542b20c01b2dd1d03bc6299d34bf2588db30
HoangLong-Lumi <111274514+HoangLong-Lumi@users.noreply.github.com>
2022-10-29 21:16:38 +0700

    a32: Import empty proprietary files list

---
+++ setup-makefiles.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#
+# Copyright (C) 2022 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# If we're being sourced by the common script that we called,
+# stop right here. No need to go down the rabbit hole.
+if [ "${BASH_SOURCE[0]}" != "${0}" ]; then
+    return
+fi
+
+set -e
+
+export DEVICE=a32
+export DEVICE_COMMON=mt6769-common
+export VENDOR=samsung
+
+"./../../${VENDOR}/${DEVICE_COMMON}/extract-files.sh" "$@"
